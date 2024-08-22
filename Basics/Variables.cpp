188d2c413371f73169f38f511ee5420839f0c119
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-22 08:30:57 +0530

    Add files via upload

---
+++ Basics/Variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<9+2<<'\n';
+cout<<8*2 <<"\n";
+int x = 90  ; // this piece of code is known as variable declaration
+cout<< x+ 5 << endl;
+int age = 18 ;
+cout<<"he's "<<age<<endl;
+int a = 4, b = 5, c = 8 ;
+cout<<a<<"\n"<<b<<"\n"<<c;
+};
+
+//  in simple language Variables can be defined as containers
