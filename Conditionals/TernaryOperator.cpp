5f4231de0a1967a029873e079c3d199a701b256c
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-25 10:01:47 +0530

    Add files via upload

---
+++ Conditionals/TernaryOperator.cpp
@@ -0,0 +1,10 @@
+// Ternary operator is a Substitute for if-else statement 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int number ;
+    cout<<"enter the number : ";
+    cin>>number ;
+    (number>10) ? cout<<"number is greater than 10 " : cout<<"number is less than 10";
+}
